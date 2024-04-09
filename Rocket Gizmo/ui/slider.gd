a3b87a1d3d8fdcde9aff83b97558840d3ee4cdcc
eweisenberg <134983393+eweisenberg@users.noreply.github.com>
2024-04-09 16:41:17 -0400

    Finished slider logic

--- Rocket Gizmo/ui/slider.gd
+++ Rocket Gizmo/ui/slider.gd
@@ -1,15 +1,17 @@
 class_name GizmoSlider
 extends MarginContainer
 
+# Instance variables for slider
 @export var text: String
 @export var min_value: int
 @export var max_value: int
 @export var step: int
 @export var default_value: int
 
-@onready var label = $Label
-@onready var slider = $HBoxContainer/HSlider
-@onready var line_edit = $HBoxContainer/LineEdit
+# Nodes it needs to access to modify or read values
+@onready var label: Label = $Label
+@onready var slider: HSlider = $HBoxContainer/HSlider
+@onready var line_edit: LineEdit = $HBoxContainer/LineEdit
 
 func _ready() -> void:
 	label.text = text
@@ -20,4 +22,25 @@ func _ready() -> void:
 	line_edit.text = str(default_value)
 
 func _on_slider_value_changed(value: float) -> void:
-	line_edit.text = str(value)
+	if !line_edit.has_focus():
+		line_edit.text = str(value)
+
+
+func _on_line_edit_text_changed(new_text: String) -> void:
+	# Return if text is not a number
+	if !line_edit.text.is_valid_int():
+		return
+	var value: int = int(line_edit.text)
+	if value >= min_value and value <= max_value:
+		slider.value = value
+	elif value < min_value:
+		slider.value = min_value
+	else:
+		slider.value = max_value
+
+
+func _on_line_edit_focus_exited() -> void:
+	if (line_edit.text == ""):
+		_on_slider_value_changed(0)
+	else:
+		_on_slider_value_changed(slider.value)
