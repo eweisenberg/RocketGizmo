af1817e6efb2ebb76756cc27a02df3fca5d06bf0
eweisenberg <134983393+eweisenberg@users.noreply.github.com>
2024-04-11 18:04:50 -0400

    In class

--- Rocket Gizmo/launch_tab_controller.gd
+++ Rocket Gizmo/launch_tab_controller.gd
@@ -7,17 +7,40 @@ extends VBoxContainer
 
 func _ready() -> void:
 	launch_dropdown.item_selected.connect(change_gravity_label)
+	goal_dropdown.item_selected.connect(change_distance_label)
 	change_gravity_label(0)
 
-func change_gravity_label(index) -> void:
+func change_gravity_label(_index: int) -> void:
 	var text: String = "Gravity: "
 	if launch_dropdown.selected == 0:
 		text += "9.8"
 	elif launch_dropdown.selected == 1:
-		text += "0"
-	elif launch_dropdown.selected == 2:
 		text += "1.62"
 	else:
 		text += "3.71"
 	text += " m/s²  "
 	gravity_label.text = text
+	change_distance_label(0)
+
+func change_distance_label(_index: int) -> void:
+	var initial_position: int
+	if launch_dropdown.selected == 0:
+		initial_position = 0
+	elif launch_dropdown.selected == 1:
+		initial_position = 384400
+	else:
+		initial_position = 225000000
+	
+	var final_position: int
+	if goal_dropdown.selected == 0:
+		final_position = 0
+	elif goal_dropdown.selected == 1:
+		final_position = 384400
+	else:
+		final_position = 225000000
+	
+	var distance: int = abs(final_position - initial_position)
+	if distance == 0:
+		distance_label.text = "Select different launch and goal locations.  "
+	else:
+		distance_label.text = str(distance) + " km away  "
